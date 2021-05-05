8b248a0b32143eb37b21a1979690fee24f24fa30
Douglas Schilling Landgraf <dougsland@gmail.com>
2021-05-05 10:22:58 -0400

    initial base64 module

---
+++ pkg/base64/base64.go
@@ -0,0 +1,52 @@
+package base64
+
+/*
+Copyright 2015 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+// GenerateRandomBytes will generate random bytes.
+//
+// Args:
+//	- Number of bytes
+//
+// Returns:
+//	-[]byte or error
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+
+// GenerateRandomString returns base64 encoded string
+//
+// Args:
+//	- number of bytes
+//
+// Returns:
+//	- string or error
+func GenerateRandomString(n int) (string, error) {
+	b, err := GenerateRandomBytes(n)
+	return base64.URLEncoding.EncodeToString(b), err
+}
