62ad9689d6a24d1c1d25a57d2184a4a600f58e9e
Douglas Schilling Landgraf <dougsland@gmail.com>
2021-05-06 22:36:20 -0400

    gofmt secret.go

--- pkg/secret/secret.go
+++ pkg/secret/secret.go
@@ -18,8 +18,8 @@ limitations under the License.
 
 import (
 	"context"
-	"strings"
 	"errors"
+	"strings"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -28,11 +28,11 @@ import (
 )
 
 type Instance struct {
-	Name string
+	Name      string
 	Namespace string
-	Type string
-	Key string
-	Value string
+	Type      string
+	Key       string
+	Value     string
 	// "k8s.io/api/core/v1/types.go"
 	// Type:
 	// Opaque				arbitrary user-defined data
@@ -53,30 +53,30 @@ type Instance struct {
 //
 // Returns:
 //     error or nil
-//     
+//
 func Create(c *client.Client, i *Instance) error {
 	secretType, err := detectSecretType(i.Type)
 	if err != nil {
 		return err
 	}
 
-        secret := v1.Secret{
-                ObjectMeta: metav1.ObjectMeta{
-                        Name:      i.Name,
-                        Namespace: i.Namespace,
-                },
-                Data: map[string][]byte{
-                        i.Key: []byte(i.Value),
-                },
-                Type: secretType,
-        }
-        _, err = c.Clientset.CoreV1().Secrets(i.Namespace).Create(
+	secret := v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      i.Name,
+			Namespace: i.Namespace,
+		},
+		Data: map[string][]byte{
+			i.Key: []byte(i.Value),
+		},
+		Type: secretType,
+	}
+	_, err = c.Clientset.CoreV1().Secrets(i.Namespace).Create(
 		context.TODO(),
 		&secret,
 		metav1.CreateOptions{})
-        if err != nil {
+	if err != nil {
 		return err
-        }
+	}
 	return nil
 }
 
@@ -88,25 +88,25 @@ func Create(c *client.Client, i *Instance) error {
 //
 // Returns:
 //     v1.Secret.Type or error
-//     
+//
 func detectSecretType(s string) (v1.SecretType, error) {
 	switch strings.ToLower(s) {
-        case "opaque":
-                return v1.SecretTypeOpaque, nil
-        case "kubernetes.io/basic-auth":
-                return v1.SecretTypeBasicAuth, nil
-        case "kubernetes.io/tls":
-                return v1.SecretTypeTLS, nil
-        case "kubernetes.io/ssh-auth":
-                return v1.SecretTypeSSHAuth, nil
-        case "kubernetes.io/service-account-token":
-                return v1.SecretTypeServiceAccountToken, nil
+	case "opaque":
+		return v1.SecretTypeOpaque, nil
+	case "kubernetes.io/basic-auth":
+		return v1.SecretTypeBasicAuth, nil
+	case "kubernetes.io/tls":
+		return v1.SecretTypeTLS, nil
+	case "kubernetes.io/ssh-auth":
+		return v1.SecretTypeSSHAuth, nil
+	case "kubernetes.io/service-account-token":
+		return v1.SecretTypeServiceAccountToken, nil
 	case "kubernetes.io/dockercfg":
 		return v1.SecretTypeDockercfg, nil
 	case "kubernetes.io/dockerconfigjson":
 		return v1.SecretTypeDockerConfigJson, nil
-        }
-        return "", errors.New("unknown secretType yet\n")
+	}
+	return "", errors.New("unknown secretType yet\n")
 }
 
 // Exists will check if thsee secret  exists or not
@@ -118,15 +118,15 @@ func detectSecretType(s string) (v1.SecretType, error) {
 //
 // Returns:
 //     string (namespace name) OR error type
-//     
+//
 func Exists(c *client.Client, secretname string, namespace string) (string, error) {
-        exists, err := c.Clientset.CoreV1().Secrets(namespace).Get(
-                context.TODO(),
-                secretname,
-                metav1.GetOptions{})
-        if err != nil {
-                return "", err
-        }
+	exists, err := c.Clientset.CoreV1().Secrets(namespace).Get(
+		context.TODO(),
+		secretname,
+		metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
 
-        return exists.Name, nil
+	return exists.Name, nil
 }
