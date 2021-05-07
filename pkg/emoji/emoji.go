a599fa7b57226545a9168f87c9c1bd2332b8c10a
Douglas Schilling Landgraf <dougsland@gmail.com>
2021-05-06 22:31:37 -0400

    gofmt emoji.go

--- pkg/emoji/emoji.go
+++ pkg/emoji/emoji.go
@@ -22,33 +22,33 @@ import (
 )
 
 type Type struct {
-	Rocket string              // 🚀
-	ThumbsUp string            // 👍
-	ThumbsDown string          // 👎
-	Collision string           // 💥
-	Robot string               // 🤖
-	AlienMonster string        // 👾 
-	Alien string               // 👽
-	Skull string               // 💀
-	SkullAndCrossBones string  // ☠
-	Ghost string               // 👻
-	AngryFace string           // 😠
-	NerdFace string            // 🤓
-	SmileFace string           // 😀  ￼
-	PartyFace string	   // 🥳 
-	SatelliteAntenna string    // 📡
-	CheckMarkButton string     // ✅
-	CrossMark string           // ❌
-	ChequeredFlag string	   // 🏁
-	MegaPhone string	   // 📣
-        Rainbow string		   // 🌈
-        HourGlassNotDone string    // ⏳
-	StopSign string		   // 🛑
-	Construction string	   // 🚧
-	RedHeart string		   // ❤
-	PileOfPoo string	   // 💩
-	ClownFace string	   // 🤡
-	SleepingFace string	   // 😴
+	Rocket             string // 🚀
+	ThumbsUp           string // 👍
+	ThumbsDown         string // 👎
+	Collision          string // 💥
+	Robot              string // 🤖
+	AlienMonster       string // 👾
+	Alien              string // 👽
+	Skull              string // 💀
+	SkullAndCrossBones string // ☠
+	Ghost              string // 👻
+	AngryFace          string // 😠
+	NerdFace           string // 🤓
+	SmileFace          string // 😀  ￼
+	PartyFace          string // 🥳
+	SatelliteAntenna   string // 📡
+	CheckMarkButton    string // ✅
+	CrossMark          string // ❌
+	ChequeredFlag      string // 🏁
+	MegaPhone          string // 📣
+	Rainbow            string // 🌈
+	HourGlassNotDone   string // ⏳
+	StopSign           string // 🛑
+	Construction       string // 🚧
+	RedHeart           string // ❤
+	PileOfPoo          string // 💩
+	ClownFace          string // 🤡
+	SleepingFace       string // 😴
 }
 
 // LoadEmojis will load emojis codes
@@ -58,7 +58,7 @@ type Type struct {
 //	https://www.unicode.org/emoji/charts/full-emoji-modifiers.html
 //
 // Args:
-//     
+//
 //      None
 //
 // Returns:
@@ -67,34 +67,34 @@ type Type struct {
 //
 func LoadEmojis() Type {
 
-	Emojis := Type {
-		Rocket: "\\U0001f680",            // 🚀
-		ThumbsUp: "\\U0001f44D",          // 👍
-		ThumbsDown: "\\U0001f44e",        // 👎
-		Collision: "\\U0001f4a5",         // 💥
-		Robot: "\\U0001f916",             // 🤖
-		AlienMonster: "\\U0001f47e",      // 👾
-		Alien: "\\U0001f47d",             // 👽
-		Skull: "\\U0001f480",             // 💀
-		SkullAndCrossBones: "\\U0002620", // ☠
-		Ghost: "\\U0001f47b",             // 👻
-		AngryFace: "\\U0001f620",         // 😠
-		NerdFace: "\\U0001f913",          // 🤓
-		SmileFace: "\\U0001f60e",         // 😀
-		PartyFace: "\\U0001f973",         // 🥳 
-		SatelliteAntenna: "\\U0001f4e1",  // 📡
-		CheckMarkButton: "\\U0002705",    // ✅
-		CrossMark: "\\U000274C",          // ❌
-		ChequeredFlag: "\\U0001f3c1",	  // 🏁
-		MegaPhone: "\\U0001f4e3",	  // 📣
-		Rainbow: "\\U0001f308",           // 🌈
-		HourGlassNotDone: "\\U00023f3",   // ⏳
-		StopSign: "\\U0001f6D1",	  // 🛑
-		Construction: "\\U0001f6a7",	  // 🚧
-		RedHeart: "\\U0002764",           // ❤
-		PileOfPoo: "\\U0001f4a9",	  // 💩
-		ClownFace: "\\U0001f921",	  // 🤡
-		SleepingFace: "\\U0001f634",	  // 😴
+	Emojis := Type{
+		Rocket:             "\\U0001f680", // 🚀
+		ThumbsUp:           "\\U0001f44D", // 👍
+		ThumbsDown:         "\\U0001f44e", // 👎
+		Collision:          "\\U0001f4a5", // 💥
+		Robot:              "\\U0001f916", // 🤖
+		AlienMonster:       "\\U0001f47e", // 👾
+		Alien:              "\\U0001f47d", // 👽
+		Skull:              "\\U0001f480", // 💀
+		SkullAndCrossBones: "\\U0002620",  // ☠
+		Ghost:              "\\U0001f47b", // 👻
+		AngryFace:          "\\U0001f620", // 😠
+		NerdFace:           "\\U0001f913", // 🤓
+		SmileFace:          "\\U0001f60e", // 😀
+		PartyFace:          "\\U0001f973", // 🥳
+		SatelliteAntenna:   "\\U0001f4e1", // 📡
+		CheckMarkButton:    "\\U0002705",  // ✅
+		CrossMark:          "\\U000274C",  // ❌
+		ChequeredFlag:      "\\U0001f3c1", // 🏁
+		MegaPhone:          "\\U0001f4e3", // 📣
+		Rainbow:            "\\U0001f308", // 🌈
+		HourGlassNotDone:   "\\U00023f3",  // ⏳
+		StopSign:           "\\U0001f6D1", // 🛑
+		Construction:       "\\U0001f6a7", // 🚧
+		RedHeart:           "\\U0002764",  // ❤
+		PileOfPoo:          "\\U0001f4a9", // 💩
+		ClownFace:          "\\U0001f921", // 🤡
+		SleepingFace:       "\\U0001f634", // 😴
 	}
 	return Emojis
 }
@@ -106,7 +106,7 @@ func LoadEmojis() Type {
 //	https://www.unicode.org/emoji/charts/full-emoji-modifiers.html
 //
 // Args:
-//     
+//
 //      string - unicode string quoted
 //
 // Returns:
