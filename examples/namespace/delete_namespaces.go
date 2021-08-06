b798c23e51a3e7e262819c8b56b17703bd3bdc32
Douglas Schilling Landgraf <dougsland@redhat.com>
2021-08-05 21:44:18 -0400

    example: delete namespaces
    Delete namespaces not in the list nsNames
    
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

---
+++ examples/namespace/delete_namespaces.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2015 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/thekubeworld/k8devel/pkg/client"
+	"github.com/thekubeworld/k8devel/pkg/namespace"
+)
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	nsNames := []string{"default", "kube-node-lease", "kube-public", "kube-system", "metallb-system", "signalfx", "tekton-pipelines", "itpaas"}
+	c := client.Client{}
+	c.NumberMaxOfAttemptsPerTask = 10
+	c.TimeoutTaskInSec = 2
+
+	// Connect to cluster from:
+	//      - $HOME/kubeconfig (Linux)
+	//      - os.Getenv("USERPROFILE") (Windows)
+	c.Connect()
+
+	nsAll, err := namespace.List(&c)
+	if err != nil {
+		fmt.Printf("exiting... failed to create: %s\n", err)
+		os.Exit(1)
+	}
+	for _, n := range nsAll.Items {
+		// DO NOT DELETE NAMESPACE in nsNames
+		if contains(nsNames, n.ObjectMeta.Name) {
+			continue
+		} else {
+			fmt.Printf("Deleting namespace: %s\n", n.ObjectMeta.Name)
+			namespace.Delete(&c, n.ObjectMeta.Name)
+		}
+	}
+}
